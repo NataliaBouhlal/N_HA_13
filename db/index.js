b79a63a1087e502e429386d29789008e28a8ab73
N Bouhlal <umkaser1984@gmail.com>
2026-03-27 09:01:04 +0100

    Добавила изменения

--- db/index.js
+++ db/index.js
@@ -7,9 +7,9 @@ export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URL);
 
-        console.log(`MongoDB connected: ${conn.connection.host} ✅`);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.error('MongoDB connection error ❌', error);
+        console.error('MongoDB connection error', error);
         process.exit(1);
     }
 };
